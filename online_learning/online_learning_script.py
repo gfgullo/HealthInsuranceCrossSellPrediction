e14e0068bea14f8826ced7ccdb41ae6b6b233a77
gfgullo <gfgullo@gmail.com>
2021-06-03 19:29:31 +0200

    online learning - test #4

--- online_learning/online_learning_script.py
+++ online_learning/online_learning_script.py
@@ -6,7 +6,7 @@ import boto3
 from botocore.exceptions import ClientError
 from datetime import datetime as dt
 import tarfile
-from os import remove,cwd
+from os import remove, getcwd
 from shutil import move, rmtree
 from os.path import abspath
 
@@ -56,7 +56,7 @@ def write_log():
     
 if __name__== "__main__":
     
-    print(os.cwd())
+    print(getcwd())
     print("Uploading collected samples on S3...", end="")
     training_data_uri = upload_data()
     print("DONE")
